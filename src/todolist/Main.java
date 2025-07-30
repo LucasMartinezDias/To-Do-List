8b46e6dadcdfc813a66e4268751e6fd2fc7d58e0
Lucas <lucasmdias05@gmail.com>
2025-07-30 18:29:01 -0300

    feat: creation of the interactive menu

--- src/todolist/Main.java
+++ src/todolist/Main.java
@@ -1,17 +1,44 @@
 package todolist;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TaskManager manager = new TaskManager();
+        int option;
+
+        do {
+            System.out.println("\n--- To-Do-List ---");
+            System.out.println("1 - Adicionar Tarefa");
+            System.out.println("2 - Listar Tarefas");
+            System.out.println("3 - Marcar como concluída");
+            System.out.println("0 - Sair");
+            System.out.print("Opção: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.print("Descrição da tarefa: ");
+                    String description = scanner.nextLine();
+                    manager.addTask(description);
+                    break;
+                case 2:
+                    manager.listTasks();
+                    break;
+                case 3:
+                    System.out.print("ID da tarefa: ");
+                    int id = scanner.nextInt();
+                    manager.markTaskCompleted(id);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (option != 0);
+
+        scanner.close();
     }
 }
